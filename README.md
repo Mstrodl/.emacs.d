8ed791f91b1a0573f0ecb0ba7fb1e3a6ad8c06d1
heatingdevice <ipadlover8322@gmail.com>
2017-11-27 17:25:35 -0500

    My modification things

--- README.md
+++ README.md
@@ -1,23 +1,14 @@
-emacs-evil-bootstrap
+.emacs.d
 ====================
 
-The quickest way to trying out Vim in Emacs
+aaa this is my emacs thing,,,
 
 Install
 =======
 
-`git clone https://github.com/bling/emacs-evil-bootstrap.git ~/.emacs.d`
-
-Settings
-========
-
-This bootstrap changes 3 evil-mode settings from their default values:
-
-*  `evil-search-module` is set to `evil-search` instead of the default `isearch` module.
-*  `evil-want-C-u-scroll` binds `C-u` to scroll up instead of the default `universal-argument` function.
-*  `evil-C-w-in-emacs-state` binds `C-w` to control windows just like Vim instead of the default of cutting a region.
+`git clone https://github.com/Mstrodl/.emacs.d.git ~/.emacs.d`
 
 License
 =======
 
-MIT License. Copyright (c) 2013 Bailey Ling.
+MIT License. Copyright (c) 2013 Bailey Ling. Copyright (c) 2017 Heatingdevice.
--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+(load-file "~/.emacs.d/elcord/elcord.el")
+;; (setq elcord-join-and-spectate t)
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
@@ -25,3 +31,36 @@ re-downloaded in order to locate PACKAGE."
 
 (require 'evil)
 (evil-mode t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" default)))
+ '(inhibit-startup-screen t)
+ '(js-indent-level 2)
+ '(package-selected-packages
+   (quote
+    (elcord package-lint rust-mode flycheck markdown-preview-mode markdown-mode+ markdown-mode wakatime-mode auto-complete rjsx-mode helm-spotify-plus helm-spotify ## dracula-theme evil)))
+ '(ring-bell-function (quote ignore))
+ '(send-mail-function (quote smtpmail-send-it))
+ '(smtpmail-smtp-server "mail.twc.com")
+ '(smtpmail-smtp-service 25)
+ '(standard-indent 2))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(setq-default indent-tabs-mode nil)
+(setq tab-width 2)
+(defvaralias 'c-basic-offset 'tab-width)
+(defvaralias 'cperl-indent-level 'tab-width)
+(defvaralias 'js-indent-level 'tab-width)
+
+(load-theme
+  'dracula)
